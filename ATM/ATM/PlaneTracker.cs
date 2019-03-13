5bbe540931baa60cd8d4b258dbdcb1fd62067604
Jannik <Jannik.lading@hotmail.dk>
2019-03-13 12:15:34 +0100

    Planetracker added methods - not done

--- ATM/ATM/PlaneTracker.cs
+++ ATM/ATM/PlaneTracker.cs
@@ -9,13 +9,28 @@ namespace ATM
 {
     class PlaneTracker
     {
+        private ITransponderReceiver receiver;
+        private List<Plane> _planes = new List<Plane>();
 
-
-        List<Plane> 
-
-        Plane ConvertStringToPlane(String planesString)
+        public PlaneTracker()
         {
 
+        }
+
+
+        private Plane ConvertStringToPlane(String planeString)
+        {
+
+            return null; 
+        }
+
+        private void TransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            
+            foreach (var data in e.TransponderData)
+            {
+                _planes.Add(ConvertStringToPlane(data));
+            }
         }
 
 
