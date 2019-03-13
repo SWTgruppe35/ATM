93bf5039d762379c0766b5c10c5f268200e34ce3
Jannik <Jannik.lading@hotmail.dk>
2019-03-13 11:06:55 +0100

    PlaneTracker instantiated

---
+++ ATM/ATM/PlaneTracker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace ATM
+{
+    class PlaneTracker
+    {
+
+
+        List<Plane> 
+
+        Plane ConvertStringToPlane(String planesString)
+        {
+
+        }
+
+
+    }
+}
