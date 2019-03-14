640eac9a47630965959960173470e967158ff34d
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-03-14 10:06:37 +0100

    Logger added

---
+++ ATM/ATM/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Logger: ILogger
+    {
+        public void LogConflictingPlanes(Plane plane1, Plane plane2)
+        {
+
+        }
+    }
+}
