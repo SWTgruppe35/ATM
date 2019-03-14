7df36e9c48ba4fc916fd451d398da95e41e94ed7
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-03-14 10:27:04 +0100

    Logger implemented

--- ATM/ATM/Logger.cs
+++ ATM/ATM/Logger.cs
@@ -10,7 +10,8 @@ namespace ATM
     {
         public void LogConflictingPlanes(Plane plane1, Plane plane2)
         {
-
+            string[] file = {plane1.Tag, plane2.Tag};
+            System.IO.File.WriteAllLines(@"C:\Users\Tobias\source\repos\SWTgruppe35\ATM\ATM\LogFile.txt", file);
         }
     }
 }
