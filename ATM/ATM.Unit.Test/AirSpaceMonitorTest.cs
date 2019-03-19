bea3126032c17e989c07556828b5f80722e198dc
Nichlaes <nichlaes.soerensen@gmail.com>
2019-03-19 15:03:31 +0100

    AirSpaceMonitorTest completed with no errors

--- ATM/ATM.Unit.Test/AirSpaceMonitorTest.cs
+++ ATM/ATM.Unit.Test/AirSpaceMonitorTest.cs
@@ -13,35 +13,63 @@ namespace ATM.Unit.Test
     [TestFixture]
     class AirSpaceMonitorTest
     {
-        
-        
-            private IAirSpaceMonitor _uut;
-            private  List<Plane> _planes;
-            private Plane _plane;
 
 
-       [SetUp]
-            public void Setup()
+        private IAirSpaceMonitor _uut;
+        private List<Plane> _planes;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new AirSpaceMonitor();
+            _planes = new List<Plane>();
+
+            for (int i = 0; i < 5; i++)
             {
-                _uut = new AirSpaceMonitor();
-                _planes = new List<Plane>(); 
-                _plane = new Plane("TestPlane",50000,50000,5000,DateTime.Now);
-        }
-
-        [TestCase(false,499)]
-        [TestCase(false,20001)]
-        [TestCase(true, 500)]
-            public void AltitudeOutOfBoundaries(bool inList,double altitude)
-            {
-                _plane.Altitude = altitude;
-
-                _planes.Add(_plane);
-
-                _uut.Monitor(_planes);
-
-                Assert.Equals( inList,_planes.Contains(_plane));
+                _planes.Add(new Plane("Plane" + i.ToString(), 50000, 50000, 5000, DateTime.Now));
             }
 
-        
+        }
+
+        [TestCase(false, 499)]
+        [TestCase(false, 20001)]
+        [TestCase(true, 500)]
+        [TestCase(true, 20000)]
+        public void AltitudeBoundaries(bool inList, double altitude)
+        {
+            Plane testPlane = new Plane("testPlane", 50000, 50000, altitude, DateTime.Now);
+            _planes.Add(testPlane);
+
+            _uut.Monitor(ref _planes);
+
+            Assert.AreEqual(inList, _planes.Contains(testPlane));
+        }
+
+        [TestCase(false, 80001)]
+        [TestCase(true, 80000)]
+        public void positionXBoundaries(bool inList, double posX)
+        {
+            Plane testPlane = new Plane("testPlane", posX, 50000, 500, DateTime.Now);
+            _planes.Add(testPlane);
+
+            _uut.Monitor(ref _planes);
+
+            Assert.AreEqual(inList, _planes.Contains(testPlane));
+        }
+
+        [TestCase(false, 80001)]
+        [TestCase(true, 80000)]
+        public void positionYBoundaries(bool inList, double posY)
+        {
+            Plane testPlane = new Plane("testPlane", 50000, posY, 500, DateTime.Now);
+            _planes.Add(testPlane);
+
+            _uut.Monitor(ref _planes);
+
+            Assert.AreEqual(inList, _planes.Contains(testPlane));
+        }
+
+
     }
 }
