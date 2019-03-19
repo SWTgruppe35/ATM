4f3ea60a48d05a97408b0fcc03707fd1347f0a8e
Berthelmaster <berthel0802@gmail.com>
2019-03-19 12:16:34 +0100

    Class1 removed

---
+++ ATM/ATM.Unit.Test/AirSpaceMonitorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace ATM.Unit.Test
+{
+    class AirSpaceMonitorTest
+    {
+        [TestFixture]
+        public class ATMWithEventsUnitTests
+        {
+            private IAirSpaceMonitor _uut;
+
+
+
+            [SetUp]
+            public void Setup()
+            {
+                _uut = new AirSpaceMonitor();
+
+            }
+
+            [Test]
+            public void AltitudeTooLow()
+            {
+                _
+            }
+
+        }
+    }
+}
