5b033298ac14c24e6149cb0641bc3df0a61f38fd
Berthelmaster <berthel0802@gmail.com>
2019-03-19 14:39:19 +0100

    Velocity updated

--- ATM/ATM.Unit.Test/AirSpaceMonitorTest.cs
+++ ATM/ATM.Unit.Test/AirSpaceMonitorTest.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
-
+/*
 namespace ATM.Unit.Test
 {
     class AirSpaceMonitorTest
@@ -35,3 +35,4 @@ namespace ATM.Unit.Test
         }
     }
 }
+*/
--- ATM/ATM.Unit.Test/Calcuate.Test.cs
+++ ATM/ATM.Unit.Test/Calcuate.Test.cs
@@ -32,24 +32,30 @@ namespace ATM.Unit.Test
         [Test]
         public void CalcuateFromOnePointEqualsZero()
         {
-            _Plane1 = new Plane("tag1", 10000, 10000, 10000, DateTime.Now);
+            _Plane1 = new Plane("tag1", 12000, 12000, 10000, DateTime.Now.AddSeconds(2));
 
             _Plane2 = new Plane("tag2", 10000, 10000, 12000, DateTime.Now);
-
-            _newPlane = new List<Plane>
-            {
-                _Plane1
-            };
-
-            _oldPlane = new List<Plane>
-            {
-                _Plane2
-            };
 
             _uut.CalculateCourse(_Plane2, _Plane1);
             Assert.That(_Plane2.CompassCourse, Is.EqualTo(0));
 
-        }   
+
+        }
+
+        [Test]
+        public void CaculateVelocityisEqualto()
+        {
+            var _date1 = new DateTime(2019, 8, 8, 2, 4, 55);
+            var _date2 = new DateTime(2019, 8, 8, 2, 4, 54);
+
+
+            _Plane1 = new Plane("tag1", 400, 400, 10000, _date1);
+
+            _Plane2 = new Plane("tag2", 300, 300, 14000, _date2);
+
+            _uut.CalculateVelocity(_Plane1, _Plane2);
+            Assert.That(_Plane1.HorizontalVelocity, Is.InRange(140, 144));
+        }
 
     }
 }
