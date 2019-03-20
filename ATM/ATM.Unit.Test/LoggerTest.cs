c7f1a7706679d6f4686245f4101ce0540c74585c
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-03-20 10:28:59 +0100

    Logger, generic path added + logger test updated with multiple planes to log - Success

--- ATM/ATM.Unit.Test/LoggerTest.cs
+++ ATM/ATM.Unit.Test/LoggerTest.cs
@@ -12,22 +12,35 @@ namespace ATM.Unit.Test
     class LoggerTest
     {
         private ATM.ILogger _logger;
-        private Plane _plane;
+        private Plane _plane1;
+        private Plane _plane2;
 
         [SetUp]
 
         public void Setup()
         {
             _logger=new Logger();
-            _plane = new Plane("plane", 50000, 20000, 18000, DateTime.Now);
+            _plane1 = new Plane("plane1", 50000, 20000, 18000, DateTime.Now);
+            _plane2 = new Plane("plane2", 50000, 20000, 18000, DateTime.Now);
+
         }
 
         [Test]
-        public void PlaneCorrectLogged()
+        public void OnePlaneCorrectLogged()
         {
-            _logger.LogConflictingPlanes(_plane);
+            _logger.LogConflictingPlanes(_plane1);
 
-            Assert.That(System.IO.File.ReadAllText(@"C:\Users\Tobias\source\repos\SWTgruppe35\ATM\ATM\LogFile.txt"), Is.EqualTo(_plane.Tag + "\r\n"));
+            Assert.That(System.IO.File.ReadAllText(@"..\LogFile.txt"), Is.EqualTo(_plane1.Tag + "\r\n"));
+        }
+
+        [Test]
+        public void MultiplePlanesCorrectLogged()
+        {
+            _logger.LogConflictingPlanes(_plane1);
+            _logger.LogConflictingPlanes(_plane2);
+
+
+            Assert.That(System.IO.File.ReadAllText(@"..\LogFile.txt"), Is.EqualTo(_plane1.Tag + "\r\n"+_plane2.Tag + "\r\n"));
         }
     }
 }
--- ATM/ATM/Logger.cs
+++ ATM/ATM/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,18 @@ namespace ATM
 {
     public class Logger: ILogger
     {
+        public Logger()
+        {
+            //_file=new StreamWriter(@"..\LogFile.txt", true);
+        }
         public void LogConflictingPlanes(Plane plane1)
         {
-            string[] file = {plane1.Tag};
-            System.IO.File.WriteAllLines(@"C:\Users\Tobias\source\repos\SWTgruppe35\ATM\ATM\LogFile.txt", file);
+            _file = new StreamWriter(@"..\LogFile.txt", true);
+            string text = plane1.Tag;
+            _file.WriteLine(text);
+            _file.Close();
         }
+
+        private StreamWriter _file;
     }
 }
