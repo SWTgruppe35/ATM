ae10e9fedc7fbed82ca64ce23cd04c8fdc378d87
Berthelmaster <berthel0802@gmail.com>
2019-03-20 10:34:28 +0100

    IRender & ConsoleRendering Done

---
+++ ATM/ATM/IRender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public interface IRender
+    {
+        void PrintPlanes(ref List<Plane> planes);
+
+        void PrintSpaceCollision(ref List<Plane> planes);
+
+
+    }
+}
